78c7d00d60250fa1ab684c0caa6d2aeb90f86731
LLeicq <1012816517@qq.com>
2020-07-18 15:13:51 +0800

    第三次测试，删除文件

--- Untitled.m
+++
@@ -1,6 +0,0 @@
-
-[x,fs] = audioread('E:\birdsound_datasets\ºìÊ÷ÁÖÄñÀàÉùÒô\myData\Pycnonotus_jocosus\XC19826-Pycnonotus_jocosus.mp3');
-start_time = 0;
-end_time = 5;
-Y_new=x((fs*start_time+1):fs*end_time,1);
-audiowrite('..\XC19826-Pycnonotus_jocosus.wav',Y_new,fs);
